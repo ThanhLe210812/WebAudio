cefa7d7921216dcba3124d790e55d92f38470700
ThanhLe <lekimthanh@kng.vn>
2019-05-07 09:07:17 +0700

    aa

--- js/recordSound.js
+++ js/recordSound.js
@@ -107,39 +107,23 @@ function gotStream(stream) {
 }
 
 function startRecord() {
-    var constraints = { audio: true, video:false }
-    navigator.mediaDevices.getUserMedia(constraints).then(function(gotStream){
-        var audioContext = new(window.AudioContext || window.webkitAudioContext)();
-        var input = audioContext.createMediaStreamSource(localStream);
-        var analyser = audioContext.createAnalyser();
-        var scriptProcessor = audioContext.createScriptProcessor();
-        // Some analyser setup
-        analyser.smoothingTimeConstant = 0;
-        analyser.fftSize = 64;
-      
-        input.connect(analyser);
-        analyser.connect(scriptProcessor);
-        scriptProcessor.connect(audioContext.destination);
-        var getAverageVolume  =  function( array){
-            var length = array.length;
-            var values = 0;
-            var i = 0;
-           for (; i < length; i++) {
-              values += array[i];
-           }
-          return values / length;
+    // get permission to use mic
+    window.navigator.getUserMedia({ audio:true }, (stream) => {
+        const audioContext = new AudioContext();
+        // get mic stream
+        const source = audioContext.createMediaStreamSource( stream );
+        const scriptNode = audioContext.createScriptProcessor(4096, 1, 1);
+        source.connect(scriptNode);
+        scriptNode.connect(audioContext.destination);
+        // output to speaker
+        // source.connect(audioContext.destination);
+
+        // on process event
+        scriptNode.onaudioprocess = (e) => {
+        // get mica data
+        console.log(e.inputBuffer.getChannelData(0))
         };
-        var onAudio = function(){
-          var tempArray = new window.Uint8Array(analyser.frequencyBinCount);
-          analyser.getByteFrequencyData(tempArray);
-          var latestFrequency = (getAverageVolume(tempArray));
-          //use latestFrequency
-        };
-        scriptProcessor.onaudioprocess = onAudio;
-      })
-      .catch(function(){
-        //Handle error
-      });
+    }, console.log);
 }
 
 
