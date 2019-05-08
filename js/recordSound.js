89cc8e36e3b4ed99e3ba6eacf8c6b8a3ee05dd44
ThanhLe <lekimthanh@kng.vn>
2019-05-08 16:36:11 +0700

    11

--- js/recordSound.js
+++ js/recordSound.js
@@ -14,7 +14,7 @@ function AudioService() {
         stopTracksAndCloseCtxWhenFinished: true,
         usingMediaRecorder: typeof window.MediaRecorder !== 'undefined',
         enableEchoCancellation: true,
-        sampleRate:192000
+        sampleRate:48000
       }
 
     this.result = {
