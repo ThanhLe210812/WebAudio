b4f1b78a3c22765263687acd9c2c1c851a0beee5
ThanhLe <lekimthanh@kng.vn>
2019-05-09 09:34:26 +0700

    13

--- js/recordSound.js
+++ js/recordSound.js
@@ -56,13 +56,17 @@ function AudioService() {
 
     this._onReceiveStream = function (stream) {
         var thisObj = this;
-        thisObj.micAudioStream = stream
-        thisObj.inputStreamNode = thisObj.audioCtx.createMediaStreamSource(thisObj.micAudioStream)
+        thisObj.micAudioStream = stream;
+        thisObj.inputStreamNode = thisObj.audioCtx.createMediaStreamSource(thisObj.micAudioStream);
         //thisObj.audioCtx = thisObj.inputStreamNode.context
         thisObj.processor = thisObj.audioCtx.createScriptProcessor(1024, 1, 1);
 
         thisObj.inputStreamNode.connect(thisObj.processor);
         thisObj.processor.connect(thisObj.audioCtx.destination);
+
+        thisObj.inputStreamNode.connect(thisObj.micGainNode);
+        thisObj.micGainNode.gain.setValueAtTime(thisObj.config.micGain, thisObj.audioCtx.currentTime);
+        //thisObj.inputStreamNode.connect(thisObj.analyserNode);
 
         /* thisObj.inputStreamNode.connect(thisObj.micGainNode)
         thisObj.micGainNode.gain.setValueAtTime(thisObj.config.micGain, thisObj.audioCtx.currentTime)
